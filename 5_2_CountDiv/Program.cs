d2349e1dffc04da5e8a50231d26bd373d74caa44
Andrii Annenko <roxton.public@gmail.com>
2024-03-16 18:14:52 +0100

    Add 5.2 CountDiv and 5.4 MinAvgSlice

---
+++ 5_2_CountDiv/Program.cs
@@ -0,0 +1,10 @@
+﻿// https://app.codility.com/demo/results/trainingP3NQBS-VC5/
+
+Console.WriteLine(CountDivisibleNumbersInRange(9, 12, 3)); // 2
+Console.WriteLine(CountDivisibleNumbersInRange(6, 11, 2)); // 3
+
+// Return the number of integers within the range [A..B] (B is inclusive) that are divisible by K
+static int CountDivisibleNumbersInRange(int A, int B, int K)
+{
+    return B / K - A / K + (A % K == 0 ? 1 : 0);
+}
---
+++ 5_4_MinAvgTwoSlice/Program.cs
@@ -0,0 +1,26 @@
+﻿// https://app.codility.com/demo/results/trainingMY9D9Y-HJW/
+
+Console.WriteLine(GetIndexOfSliceWithMinAvg([4, 2, 2, 5, 1, 5, 8])); // 1
+Console.WriteLine(GetIndexOfSliceWithMinAvg([4, 2, 2, 5, -4, 5, 8])); // 3
+Console.WriteLine(GetIndexOfSliceWithMinAvg([-3, -5, -8, -4, -10])); // 2
+
+static int GetIndexOfSliceWithMinAvg(int[] A)
+{
+    var minAvg = decimal.MaxValue;
+    var index = 0;
+    for (var i = 0; i < A.Length - 2; i++)
+    {
+        var avg1 = (decimal)(A[i] + A[i + 1]) / 2;
+        var avg2 = (decimal)(A[i] + A[i + 1] + A[i + 2]) / 3;
+        var avg = Math.Min(avg1, avg2);
+        if (avg < minAvg)
+        {
+            minAvg = avg;
+            index = i;
+        }
+    }
+
+    return (decimal)(A[A.Length - 2] + A[A.Length - 1]) / 2 < minAvg
+        ? A.Length - 2
+        : index;
+}
