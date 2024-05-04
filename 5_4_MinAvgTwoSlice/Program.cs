b03121851b1fa85d4217581a72ec20523155663e
Andrii Annenko <roxton.public@gmail.com>
2024-05-04 10:22:53 +0200

    Add description to 5.4 MinAvgTwoSlice, add 6.1 Distinct

--- 5_4_MinAvgTwoSlice/Program.cs
+++ 5_4_MinAvgTwoSlice/Program.cs
@@ -4,6 +4,10 @@ Console.WriteLine(GetIndexOfSliceWithMinAvg([4, 2, 2, 5, 1, 5, 8])); // 1
 Console.WriteLine(GetIndexOfSliceWithMinAvg([4, 2, 2, 5, -4, 5, 8])); // 3
 Console.WriteLine(GetIndexOfSliceWithMinAvg([-3, -5, -8, -4, -10])); // 2
 
+// Find an index of a slice of A with a minimal average.
+// Hint! Check only the slices of 2 and 3 items.
+//       A slice longer than 3 items has its own slice of 2 or 3 items
+//       with its average <= to the average of the original slice.
 static int GetIndexOfSliceWithMinAvg(int[] A)
 {
     var minAvg = decimal.MaxValue;
---
+++ 6_1_Distinct/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine(GetDistinctElementsCount([2, 1, 1, 2, 3, 1]));
+
+// Count distinct elements
+static int GetDistinctElementsCount(int[] A)
+{
+    return new HashSet<int>(A).Count;
+}
