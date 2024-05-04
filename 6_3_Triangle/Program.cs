bc5e1b93a040ad80c908e828d09bee58a7be0bf3
Andrii Annenko <roxton.public@gmail.com>
2024-05-04 13:38:55 +0200

    Remove redundant properties from csproj files, add 6.3 Triangle

---
+++ 6_3_Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿// https://app.codility.com/demo/results/training677AXM-8CA/
+
+Console.WriteLine(IsTriangleInArray([10, 2, 5, 1, 8, 20]));
+
+// Array A contains lengths of sides of a potential triangle.
+// Check if a triangle can actually be made of any three of them.
+// Hint! Sort the array and check if any two adjacent sides
+//       are longer toghether than the next side.
+static int IsTriangleInArray(int[] A)
+{
+    if (A.Length < 3)
+        return 0;
+
+    Array.Sort(A);
+    for (int i = 0; i < A.Length - 2; i++)
+        if (A[i] > A[i + 2] - A[i + 1])
+            return 1;
+
+    return 0;
+}
