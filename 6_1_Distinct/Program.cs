fa451a864eeb510d494cd4ecbd0ae52d17d9e346
Andrii Annenko <roxton.public@gmail.com>
2024-05-04 10:26:06 +0200

    Add Codlility results link to 6.1 Distinct

--- 6_1_Distinct/Program.cs
+++ 6_1_Distinct/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine(GetDistinctElementsCount([2, 1, 1, 2, 3, 1]));
+﻿// https://app.codility.com/demo/results/trainingBPWQ9F-5T7/
+
+Console.WriteLine(GetDistinctElementsCount([2, 1, 1, 2, 3, 1]));
 
 // Count distinct elements
 static int GetDistinctElementsCount(int[] A)
