93e4918ec81acc89a08b9c943176499700af2273
Andrii Annenko <roxton.public@gmail.com>
2024-05-07 17:32:19 +0200

    Add 9.1 MaxProfit

---
+++ 9_1_MaxProfit/Program.cs
@@ -0,0 +1,22 @@
+﻿// https://app.codility.com/demo/results/training54YBXJ-NWX/
+
+Console.WriteLine(GetMaxProfit([23171, 21011, 21123, 21366, 21013, 21367])); // 356
+
+// Array A contains daily prices of a stock share.
+// Find best profit if a share was bought on one day and sold on the other.
+static int GetMaxProfit(int[] A)
+{
+    var streak = 0;
+    var best = 0;
+    for (int i = 1; i < A.Length; i++)
+    {
+        streak += A[i] - A[i - 1];
+        if (streak < 0)
+            streak = 0;
+
+        if (best < streak)
+            best = streak;
+    }
+
+    return best > 0 ? best : 0;
+}
